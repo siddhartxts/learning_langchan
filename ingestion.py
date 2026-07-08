727dca4dc14bbe5ff274a0be93ab45c85d8375da
siddharth <siddharth.muramkar1@gmail.com>
2026-07-08 06:23:07 +0530

    data indexing

--- ingestion.py
+++ ingestion.py
@@ -1,10 +1,27 @@
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
+from langchain_unstructured import UnstructuredLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_text_splitters import CharacterTextSplitter
 
 load_dotenv()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Ingesting...")
-    print(os.environ['PINECONE_API_KEY'])
+    loader = UnstructuredLoader(file_path="/Users/siddharth/Desktop/langchain-course-main/mediumblog1.txt", chunking_strategy="basic", max_characters=1000000)
+    document = loader.load()
 
+    print("splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+    print("ingesting...")
+    PineconeVectorStore.from_documents(
+        texts, embeddings, index_name=os.environ["INDEX_NAME"]
+    )
+    print("finish")
